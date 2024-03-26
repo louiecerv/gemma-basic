05fefa90bfdf50aade1acdbdad61f4b1a21fd64c
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-26 06:55:16 +0000

    Save

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ import pandas as pd
 import streamlit as st
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-access_token =  st.secrets["HUGGINGFACE_API_KEY"]
-
 document_text = "SpaceX: Aiming for the Stars SpaceX is driven by an ambitious mission and vision\
 for the future of space exploration. Here's a breakdown of their key goals and objectives: \
 Mission:  Revolutionize space technology, with the ultimate goal of enabling people to live \
